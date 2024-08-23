4ffbc930e81bce4d33a5cc4643b7179da98b5c4f
Kevin Wojcik <kwojcik@ontra.ai>
2024-08-23 16:59:24 -0700

    still crashing

--- test/foo3.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("foo", () => {
-  for (let i = 0; i < 1000; i++) {
-    it(`should work ${i}`, () => {
-      expect(i).toBe(i);
-    });
-  }
-});
